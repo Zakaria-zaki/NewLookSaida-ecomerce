9f9147a43031bed0e7de4f18fde92968e03d19a7
etudiant <etudiant@inventaireDeb10Portable.uang>
2021-01-13 00:11:11 +0100

    ajout de projet 7

---
+++ social.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>zzz</title>
+	<link rel="stylesheet" type="text/css" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" type="text/css" href="font-awsome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="styles/stylesoczial.css">
+</head>
+<body>
+    <a href="#" class="fa fa-facebook"></a>
+    <a href="#" class="fa fa-twitter"></a>
+    <a href="#" class="fa fa-instagram"></a>
+    <a href="#" class="fa fa-google"></a>
+</body>
+</html>
+
+
+
+
+
